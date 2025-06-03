b1852cb3aac88acaad61a0367101d5983266233c
yeonjisoo <yeonjisoo@outlook.com>
2025-06-03 14:28:18 +0700

    feat: added VitePress to create docs and component showcase

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vitepress'
+import tailwindcss from "@tailwindcss/vite"
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Ambrosy UI Docs",
+  description: "Sassy Description",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Form Elements',
+        items: [
+          { text: 'Button', link: '/button' }
+        ]
+      },
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/hydrotinker/ambrosy-ui' }
+    ]
+  },
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  }
+})
---
+++ docs/button.md
@@ -0,0 +1,17 @@
+<script setup>
+import Button from '../src/components/Button.vue'
+</script>
+
+# Button
+
+## Import
+
+```vue
+import Button from 'ambrosy-ui/button';
+```
+
+## Basic
+
+<Button
+    label="Submit"
+/>
